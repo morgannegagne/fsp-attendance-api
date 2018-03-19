81164ac9103c8fe666d3ab7deebc9a26d3c4d2d5
morgannegagne <morgannegagne@gmail.com>
2018-03-19 08:53:02 -0400

    fixed signins migration

--- db/migrate/20180316011300_create_signins.rb
+++ db/migrate/20180316011300_create_signins.rb
@@ -2,7 +2,7 @@ class CreateSignins < ActiveRecord::Migration[5.1]
   def change
     create_table :signins do |t|
       t.date :date
-      t.time :string
+      t.string :time
       t.integer :user_id
     end
   end
